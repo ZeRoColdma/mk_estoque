eeb1b587661939c9f1be72675fec7e4a7ecfce1f
Carlos Alberto Silva Junior <ccarloos9@gmail.com>
2022-12-04 14:31:22 -0300

    add Local Dev

--- knexfile.ts
+++ knexfile.ts
@@ -6,13 +6,11 @@ dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: process.env.DB_CLIENT,
+    client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      port: 5432,
+      database: "mk_estoque",
+      user: "postgres",
+      password: "postgres",
     },
     pool: {
       min: 2,
