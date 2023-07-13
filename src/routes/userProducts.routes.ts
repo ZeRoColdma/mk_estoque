f7a16bdcb543d0f7729658e09441b5eeaceb3a4f
Carlos <ccarloos9@gmail.com>
2023-07-13 09:08:55 -0300

    Revert "New Files Ainda Middlewares"

--- src/routes/userProducts.routes.ts
+++ src/routes/userProducts.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UserProductsController } from "../Controllers/UserProducts/UserProductsController";
+import { checkJwt } from "../middleware/decodeAuthJwt";
 const userProductsRouter = Router();
 
 const userProductsController = new UserProductsController();
@@ -7,30 +8,30 @@ const userProductsController = new UserProductsController();
 //! New User Products
 userProductsRouter.get(
   "/user_products/index/:user_id",
-
-  userProductsController.index
+  checkJwt,
+  userProductsController.index,
 );
 
 userProductsRouter.get(
   "/user_products/:id",
-
-  userProductsController.show
+  checkJwt,
+  userProductsController.show,
 );
 
 userProductsRouter.post(
   "/user_products/products",
-
-  userProductsController.create
+  checkJwt,
+  userProductsController.create,
 );
 userProductsRouter.put(
   "/user_products",
-
-  userProductsController.update
+  checkJwt,
+  userProductsController.update,
 );
 userProductsRouter.delete(
   "/user_products",
-
-  userProductsController.delete
+  checkJwt,
+  userProductsController.delete,
 );
 
 export { userProductsRouter };
